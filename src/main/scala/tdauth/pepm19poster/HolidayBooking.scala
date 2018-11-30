17ecf87bf8513e7c88fff3281319291c8695037d
Tamino Dauth <tamino@cdauth.eu>
2018-11-30 15:45:04 +0100

    Fix names

--- src/main/scala/tdauth/pepm19poster/HolidayBooking.scala
+++ src/main/scala/tdauth/pepm19poster/HolidayBooking.scala
@@ -4,22 +4,22 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 /**
-  * We want to find the best location in the Swiss for holidays but we have a limited budget in EUR.
+  * We want to find the best location in Switzerland for holidays but we have a limited budget in EUR.
   * Unfortunately, the price is only written in CHR, so we have to check for the currency rating using another server.
-  * If the Swiss is too expensive, we will go to the USA with the same check.
+  * If Switzerland is too expensive, we will go to the USA with the same check.
   * If both locations are too expensive, we stay at home and write our family that we need more money.
   *
   * Future results emulate the requests to external servers.
-  * Just imagine we had two different servers for holiday locations in the Swiss and USA.
+  * Just imagine we had two different servers for holiday locations in Switzerland and the USA.
   * Besides, there is a server which calculates the currency rating.
   */
 object HolidayBooking extends App {
   val budgetEUR = 600
-  val x1 = getSwissHolidayLocation()
-    .flatMap(getCurrencyRating)
+  val x1 = holidayLocationSwitzerland()
+    .flatMap(currencyRating)
     .filter(budgetIsSufficient)
-  val x2 = getUSAHolidayLocation()
-    .flatMap(getCurrencyRating)
+  val x2 = holidayLocationUSA()
+    .flatMap(currencyRating)
     .filter(budgetIsSufficient)
   val x3 = x1.fallbackTo(x2) // L1
   val x4 = x3.map(bookHoliday).recover(dontBookAnything) // L2
@@ -30,10 +30,12 @@ object HolidayBooking extends App {
   println(s"Copy 1: $copy1")
   println(s"Copy 2: $copy2")
 
-  def getSwissHolidayLocation() = Future { HolidayLocation(800, "Swiss", "CHF") } // Make both prices higher to don't book anything.
-  def getUSAHolidayLocation() = Future { HolidayLocation(600, "USA", "USD") } // Make the price higher for the USA, to get Swiss as result.
+  // Make the price higher for the USA, to get Switzerland as result.
+  def holidayLocationSwitzerland() = Future { HolidayLocation(600, "Switzerland", "CHF") }
+  // Make both prices higher to don't book anything.
+  def holidayLocationUSA() = Future { HolidayLocation(600, "the USA", "USD") }
 
-  def getCurrencyRating(location: HolidayLocation) = location match {
+  def currencyRating(location: HolidayLocation) = location match {
     case HolidayLocation(_, _, "CHF") => Future { HolidayLocationAndRating(location, 1.13) }
     case HolidayLocation(_, _, "USD") => Future { HolidayLocationAndRating(location, 1.14) }
     case _                            => Future.failed(new RuntimeException("Unknown currency!"))
